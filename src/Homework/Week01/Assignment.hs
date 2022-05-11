00662719e6c79946a12d0c16995b37142b5079cf
Matthew Boeh <m@mboeh.com>
2022-05-11 19:37:38 +0000

    Week 1 homework (except Hanoi)

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -1,27 +1,40 @@
 module Homework.Week01.Assignment where
 
+import Control.Monad (mfilter)
+import Data.List (unfoldr)
+import Data.Text.Internal.Builder.Int.Digits (digits)
+import Data.Tuple (swap)
+import Test.QuickCheck (NonEmptyList (NonEmpty))
+
 -- #1a
 toDigits :: Integer -> [Integer]
-toDigits = undefined
+toDigits = reverse . toDigitsRev
 
 -- #1b
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = undefined
+toDigitsRev = unfoldr digits
+  where
+    digits n = splitN <$> mfilter (> 0) (Just n)
+    splitN n = swap $ divMod n 10
 
 -- #2
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther = reverse . double . reverse
+  where
+    double (a : b : r) = a : (b * 2) : double r
+    double x = x
 
 -- #3
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits x = sum $ map (sum . toDigits) x
 
 -- #4
 validate :: Integer -> Bool
-validate = undefined
+validate n = sumDigits (doubleEveryOther $ toDigits n) `mod` 10 == 0
 
 -- #5
 type Peg = String
+
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
